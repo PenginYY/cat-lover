f10c95ac3a358eeaefd55ac7f5ee5963050241de
PattiyaY <pattiya.2547@icloud.com>
2024-09-06 09:34:51 +0700

    fix: indent in favorite, login and register

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import Navbar from "../components/Navbar";
-
+import Navbar from "../Components/Navbar";
 
 export default function Login() {
   return (
@@ -11,12 +10,31 @@ export default function Login() {
         <h3>Login Page</h3>
         <hr className="my-3" />
         <form action="">
-          <input className="block bg-gray-200 p-2 my-2 rounded-md" type="email" placeholder="Enter your email" />
-          <input className="block bg-gray-200 p-2 my-2 rounded-md" type="password" placeholder="Enter your password" />
-          <button type="submit" className="bg-green-500 p-2 rounded-md text-white">Sign In</button>
+          <input
+            className="block bg-gray-200 p-2 my-2 rounded-md"
+            type="email"
+            placeholder="Enter your email"
+          />
+          <input
+            className="block bg-gray-200 p-2 my-2 rounded-md"
+            type="password"
+            placeholder="Enter your password"
+          />
+          <button
+            type="submit"
+            className="bg-green-500 p-2 rounded-md text-white"
+          >
+            Sign In
+          </button>
         </form>
         <hr className="my-3" />
-        <p>Already have an account? go to <Link className="text-blue-500 hover:underline" href={"/Register"}>Register</Link> Page</p>
+        <p>
+          Already have an account? go to{" "}
+          <Link className="text-blue-500 hover:underline" href={"/Register"}>
+            Register
+          </Link>{" "}
+          Page
+        </p>
       </div>
     </main>
   );
