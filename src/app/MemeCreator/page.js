0db4c920536f25bfb8edcd22173d5c83d61ffb70
PattiyaY <pattiya.2547@icloud.com>
2024-09-09 22:53:59 +0700

    feat: add drop down list font color and font size and search using keyword function

--- src/app/MemeCreator/page.js
+++ src/app/MemeCreator/page.js
@@ -1,7 +1,91 @@
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import { Description, Field, Label, Select } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import clsx from "clsx";
+
 export default function MemeCreator() {
+  const [message, setMessage] = useState("");
+  const searchHandle = () => {};
+  const messageHandle = (e) => {
+    const newMessage = e.target.value;
+
+    // Log the input value and update the state
+    console.log(newMessage);
+
+    // Check if the length of the message exceeds 20 characters
+    if (newMessage.length >= 20) {
+      alert("The message limit at 20 characters!");
+    }
+
+    // Update the message state
+    setMessage(newMessage);
+  };
+
+  // List of dropdown items
+  const items = [
+    { name: "About" },
+    { name: "Base" },
+    { name: "Blog" },
+    { name: "Contact" },
+    { name: "Custom" },
+    { name: "Support" },
+    { name: "Tools" },
+  ];
+
+  // console.log(searchName);
+  // Filtered items based on the search term
+  const filteredItems = items.filter((item) =>
+    item.name.toLowerCase().includes(message.toLowerCase())
+  );
+  console.log(filteredItems);
+
   return (
-    <main>
-      <h1>MemeCreator page</h1>
+    <main className="flex flex-col h-h-dvh">
+      <div className="relative w-full h-64 md:h-96 lg:h-[1000px]">
+        <Image
+          src="/img/default-image.png"
+          alt="Default picture"
+          fill
+          className="object-cover"
+        />
+      </div>
+
+      <div className="flex justify-center items-center space-x-4 mt-10">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="35"
+          height="35"
+          fill="currentColor"
+          className="bi bi-search"
+          viewBox="0 0 16 16"
+        >
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+        </svg>
+        <input
+          className="bg-[#EAEAEA] text-center rounded w-1/3 h-10 border border-[#888]"
+          type="text"
+          placeholder="Search using keywords"
+          maxLength="20"
+          onChange={(e) => messageHandle(e)}
+        />
+        <button
+          onClick={searchHandle()}
+          className="bg-black rounded text-white w-20 h-10"
+          type="submit"
+        >
+          Search
+        </button>
+      </div>
+
+      {filteredItems.map((item) => {
+        return (
+          <p key={item.name} className="inline-block text-center">
+            {item?.name}
+          </p>
+        );
+      })}
     </main>
   );
 }
