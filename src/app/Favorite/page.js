025a5ce335496fe813d7dbea4e9a006ce13760bd
YUMI YOSHIDA <yumi.yoshida.t@gmail.com>
2024-09-16 20:26:28 +0700

    feat(models): favorite.js

--- src/app/Favorite/page.js
+++ src/app/Favorite/page.js
@@ -1,12 +1,20 @@
-import React from "react";
-import Link from "next/link";
+"use client"
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
 import Navbar from "../components/Navbar";
 
+
 export default function Favorite() {
+  const [favorites, setFavorites] = useState([]);
+  const [error, setError] = useState("");
+
+  useState(() => {
+  })
+
   return (
     <main>
       <Navbar />
-      <h1>Favorite page</h1>
+      <h1 className="text-center text-2xl font-bold my-5">Favorite Cat Memes</h1>
       <div></div>
       <div></div>
     </main>
