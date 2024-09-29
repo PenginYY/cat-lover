12fdab8be796cd3c9695da655da206259ff88225
YUMI YOSHIDA <yumi.yoshida.t@gmail.com>
2024-09-29 11:03:42 +0700

    fix: navbar add created meme button

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -65,21 +65,38 @@ export default function Navbar({ session }) {
                     href={"/CatMeme"}
                     className="flex items-center bg-white-500 text-white py-2 px-3 rounded-md text-lg my-2 hover:bg-white hover:text-black"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
+                    <svg xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
                       stroke-width="1.5"
                       stroke="currentColor"
-                      className="size-6"
-                    >
+                      class="size-6">
                       <path
                         stroke-linecap="round"
                         stroke-linejoin="round"
-                        d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+                        d="M12 4.5v15m7.5-7.5h-15"
                       />
                     </svg>
                     Meme Creator
+                  </Link>
+                </li>
+                <li className="mx-3">
+                  <Link
+                    href={"/Favorite"} // change to created meme or history page
+                    className="flex items-center bg-white-500 text-white py-2 px-3 rounded-md text-lg my-2 hover:bg-white hover:text-black"
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="size-6">
+                      <path stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0-3-3m3 3 3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
+                      />
+                    </svg>
+                    Created Meme
                   </Link>
                 </li>
                 <li className="mx-3">
